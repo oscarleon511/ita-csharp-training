e41330b459d3c2fd330c02dda56b8a3adc8a844b
Jason Wollam <jason.wollam@agilethought.com>
2016-06-21 15:47:34 -0400

    Initial commit.

---
+++ ite-chsharp-training-tests/FizzBuzzMainTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ita_chsharp_training;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ite_chsharp_training_tests
+{
+    [TestClass]
+    public class FizzBuzzMainTests
+    {
+        private FizzBuzzMain _classUnderTest;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _classUnderTest = new FizzBuzzMain();
+        }
+
+        [TestMethod]
+        public void ClassUnderTest_Is_Intialized_And_Is_Of_The_Right_Type()
+        {
+            Assert.IsInstanceOfType(_classUnderTest, typeof(FizzBuzzMain));
+        }
+
+        [TestMethod]
+        public void The_ExecuteFizzBuzz_Method_Executes_Without_Exception()
+        {
+            //Arrange
+            string result;
+            _classUnderTest.Iterations = 1;
+
+
+            //Act
+            result = _classUnderTest.ExecuteFizzBuzz();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Contains("FizzBuzz"));
+        }
+    }
+}
